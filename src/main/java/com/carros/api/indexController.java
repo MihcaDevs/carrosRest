45910bb1a12a74a08f7888e43e90c032625d0e8a
Alberto Costa <mihcadevs@gmail.com>
2019-06-09 11:32:12 -0300

    Criacao e configuracao do Banco de dados

--- src/main/java/com/carros/api/indexController.java
+++ src/main/java/com/carros/api/indexController.java
@@ -1,9 +1,6 @@
 package com.carros.api;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,21 +10,6 @@ public class indexController {
 	
 	@GetMapping
 	public String get()  {
-		return "Get Spring Boot";
-	}
-	
-	@PostMapping()
-	public String post() {
-		return "Post Spring Boot";
-	}
-	
-	@PutMapping()
-	public String put() {
-		return "Put Spring Boot";
-	}
-	
-	@DeleteMapping()
-	public String delete() {
-		return "Delete Spring Boot";
+		return "Api dos carros";
 	}
 }
---
+++ src/main/java/com/carros/domain/CarroService.java
@@ -0,0 +1,21 @@
+package com.carros.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarroService {
+	public List<Carro> getCarros() {
+		List<Carro> carros = new ArrayList<>();
+		
+		carros.add(new Carro(1L, "Fusca"));
+		carros.add(new Carro(2L, "Brasil"));
+		carros.add(new Carro(3L, "Chevette"));
+		
+		return carros;
+		
+	}
+
+}
