baea4c5873d6e1a937221e6d2bc6a30d61a05a78
Alberto Costa <mihcadevs@gmail.com>
2019-06-09 23:17:04 -0300

    Atualizar e Delete

--- src/main/java/com/carros/domain/CarroService.java
+++ src/main/java/com/carros/domain/CarroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 public class CarroService {
@@ -26,9 +27,46 @@ public class CarroService {
 		return rep.findByTipo(tipo);
 	}
 	
-	public Carro save(Carro carro) {
+	public Carro insert(Carro carro) {
+		Assert.isNull(carro.getId(), "Não foi possível atualizar o registro");
+		
 		return rep.save(carro);
 	}
 	
+	public Carro update(Carro carro, Long id) {
+		Assert.notNull(id, "Não foi possivel atualizar o registro");
+		
+		//Busca o carro no banco de dados
+		Optional<Carro> optional = getCarroById(id);
+		if(optional.isPresent()) {
+			Carro db = optional.get();
+			
+		//Copiar as propriedades
+			db.setName(carro.getName());
+			db.setTipo(carro.getTipo());
+			System.out.println("Carro id " + db.getId());
+			
+			//Atualiza o carro
+			rep.save(db);
+			
+			return db;
+			
+		} else {
+			throw new RuntimeException("Não foi possivel atualizar o registro");
+		}
+	}
 	
-}
+	public void delete(Long id) {
+		//Optional<Carro> carro = getCarroById(id);
+		//if(Carro.isPresent()) {
+			rep.deleteById(id);
+		}
+		
+	}
+	
+	//public Carro save(Carro carro) {
+		//return rep.save(carro);
+	//}
+	
+	
+
